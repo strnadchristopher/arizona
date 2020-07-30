39604aee5b8c37db6548c1648172119b336b9773
Chris Strnad <strnadchristopher@gmail.com>
2020-07-29 22:25:01 -0500

    Spotify and Weather support

---
+++ README.md
@@ -0,0 +1,24 @@
+Arizona is a fully customizable personal assistant with weather and Spotify integration*.
+
+Usage:
+To Add or remove inputs for conversation, edit the 'inputs.txt' and 'responses.txt'
+Every line in 'inputs.txt' corresponds to the same line in 'responses.txt'. Separate alternate versions of inputs or responses with a '/'
+
+Shortcuts:
+Shift-Ctrl-A - Pull up assistant
+Escape - Hide assistant
+F5 - Refresh Assistant
+
+Full Features To Be Added:
+Voice Control
+Config menu to set
+ - Your name
+ - Your city (for weather)
+ - The name of your assistant
+ - Colors and more
+ Running python scripts on command
+
+Known Bugs:
+When window is re-opened from being hidden, a strange shadow appears behind the app that doesn't go away.
+
+*only available on macOS currently.
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron')
+const { app, BrowserWindow, Menu, Tray, screen, net } = require('electron')
 const electron = require('electron')
 const globalShortcut = electron.globalShortcut
-
 const fs = require("fs");
-
+var execa = require("execa");
 var currentInput;
 
 // Asynchronous read inputs and outputs
@@ -25,28 +24,35 @@ fs.readFile('q&a/responses.txt', function (err, data) {
   //console.log("Asynchronous read: " + data.toString());
 });
 
+var loaded = false;
 
 
 var path = require('path')
 //var url = require('url')
-var iconpath = path.join(__dirname, 'icon.png') // path of y
-
+var iconpath = path.join(__dirname, 'extraResources', 'icon.png') // path of y
+var serviceScript = path.join(__dirname, 'extraResources','service.js');
 // Synchronous read
 //var data = fs.readFileSync('input.txt');
 //console.log("Synchronous read: " + data.toString());
 //console.log("Program Ended");
 var win;
-function createWindow () {
 
+function createWindow () {
+  const windowWidth = 400;
+  const windowHeight = 400;
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  const xOffset = 15;
+  const winX = width - windowWidth - xOffset;
+  const winY = height - windowHeight;
   // Create the browser window.
   win = new BrowserWindow({
     width: 400,
-    height: 500,
+    height: 400,
 	transparent: true,
 	frame: false,
-	x:3430,
-	y:580,
-  icon: './icon.png',
+	x:winX,
+	y:winY,
+  icon: iconpath,
 	alwaysOnTop:true,
 	resizable:false,
     webPreferences: {
@@ -56,7 +62,7 @@ function createWindow () {
 
 
 
-    var appIcon = new Tray("./icon.png")
+    var appIcon = new Tray(iconpath)
     //appIcon.setPressedImage
     var contextMenu = Menu.buildFromTemplate([
         {
@@ -79,9 +85,19 @@ function createWindow () {
       win.hide();
   });
 
+  win.on('blur', function(event){
+    //event.preventDefault();
+    win.hide()
+  })
+
+  win.on('show', function(event){
+    //win.hid()
+    //createWindow()
+  })
+
   win.on('close', function (event) {
       if(!app.isQuiting){
-          event.preventDefault();
+          //event.preventDefault();
           win.hide();
       }
 
@@ -91,22 +107,30 @@ function createWindow () {
 
 	//Add refresh shortcut
   globalShortcut.register('ctrl+shift+a', function() {
-		console.log('App Refreshed')
+		console.log('Bringing back app')
 		win.show();
 	})
 	globalShortcut.register('f5', function() {
 		console.log('App Refreshed')
+    //win.hide()
 		win.reload()
 	})
 	globalShortcut.register('escape', function() {
-		console.log('Leaving')
+		console.log('Hiding app')
 		//app.quit();
-    win.hide();
+    //win.close();
 	})
 	globalShortcut.register('f7', function() {
 		console.log('Showing console')
 		win.webContents.openDevTools()
 	})
+  globalShortcut.register('f2', function() {
+		//nextTrack();
+	})
+  globalShortcut.register('f3', function() {
+		getArtwork();
+	})
+
 
 
 
@@ -116,34 +140,119 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile('index.html')
   // Open the DevTools.
+  loaded = true;
+}
+var artworkURL;
+var spotifyUpdateInterval = 3; //In seconds
+var t=setInterval(checkSpotifyArt,spotifyUpdateInterval * 1000);
+function checkSpotifyArt(){
+  getArtwork();
+}
+async function getArtwork(){
+  const {stdout} = await execa('osascript', ['-e',
+  'tell application "Spotify" to return current track\'s artwork url']);
+  if(stdout != "artworkURL"){
+    artworkURL = stdout;
+    win.webContents.send('artwork', stdout);
+    getTrackInfo();
+  }
 
 }
 
+async function getTrackInfo(){
+  const [artist, title] = await Promise.all([getArtist(), getTrackTitle()]);
+    win.webContents.send('trackInfo', artist + ";" + title);
+}
+async function getArtist(){
+  const {stdout} = await execa('osascript', ['-e',
+  'tell application "Spotify" to return current track\'s artist']);
+  //console.log(art);
+  return stdout;
+}
+async function getTrackTitle(){
+  const {stdout} = await execa('osascript', ['-e',
+  'tell application "Spotify" to return current track\'s name']);
+  return stdout;
+}
 
-
-
-//console.log(stringSimilarity.compareTwoStrings("healed", "salad"));
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
 
+/*function authorizeSpotify(){
+/*
+  const request = net.request({
+    method: 'GET',
+    protocol: 'https:',
+    hostname: 'spotify.com',
+    port: 443,
+    path: "/authorize?client_id=eb0929c190354d7ea0b7e8a065ad68ed&response_type=code&redirect_uri=https://localhost/callback&scope=user-read-private%20user-read-email&state=34fFs29kd09'"
+  });
 
+  request.on('response', (response) => {
+  console.log(`STATUS: ${response.statusCode}`);
+  response.on('error', (error) => {
+    console.log(`ERROR: ${JSON.stringify(error)}`)
+  })
+})
+
+
+  request.on('login', (authInfo, callback) => {
+    console.log("Logging in");
+    callback('chris.strnad', 'Dixiegreenwood1!')
+
+})
+
+
+  var authWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      show: false,
+      'node-integration': false,
+      'web-security': false
+  });
+  // This is just an example url - follow the guide for whatever service you are using
+  var authUrl = 'https://accounts.spotify.com/authorize?client_id=eb0929c190354d7ea0b7e8a065ad68ed&response_type=code&redirect_uri=http://localhost:8888&scope=user-read-private%20user-read-email&state=34fFs29kd09'
+
+  authWindow.loadURL(authUrl);
+  authWindow.show();
+  var keyWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      show: false,
+      'node-integration': false,
+      'web-security': false
+  });
+  keyWindow.loadURL("http://localhost:8888")
+  keyWindow.show()
+  // 'will-navigate' is an event emitted when the window.location changes
+  // newUrl should contain the tokens you need
+  authWindow.webContents.on('will-navigate', function (event, newUrl) {
+      console.log(newUrl);
+
+      // More complex code to handle tokens goes here
+  });
+
+  authWindow.on('closed', function() {
+      authWindow = null;
+  });
+
+}
+*/
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    //app.quit()
+    app.quit()
   }
 })
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
+  win.show()
+  win.webContents.focus();
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    //createWindow()
   }
 })
 
@@ -161,7 +270,7 @@ function readAnswers(message){
 const { Worker, isMainThread } = require("worker_threads");
 
 function runService(workerData) {
-  const worker = new Worker("./service.js", { workerData });
+  const worker = new Worker(serviceScript, { workerData });
   //worker.postMessage(workerData);
   worker.on("message", incoming => readAnswers(incoming));
   worker.onerror = function (event) {
@@ -176,11 +285,11 @@ function runService(workerData) {
 
 
 async function run() {
-const result = runService({currentInput,inputs,responses});
+  const result = runService({currentInput,inputs,responses});
   //console.log({ isMainThread });
 }
 
-run().catch(err => console.error(err));
+//run().catch(err => console.error(err));
 
 
 const { ipcMain } = require('electron')
